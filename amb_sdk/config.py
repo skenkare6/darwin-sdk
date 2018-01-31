421c04f8ebc17548eae9722d0a241b02930e95c2
Sheila Cheng <scheng@sparkcognition.com>
2018-01-31 16:40:05 -0600

    set to prod server

--- amb_sdk/config.py
+++ amb_sdk/config.py
@@ -1,5 +1,4 @@
 
 class Config:
-    # server_url = 'http://localhost:5000/v1/'
-    server_url = 'https://amb-api.sparkcognition.com/v1/'
-    api_key = 'RsJ74ZS5AvwznbHh0AfVSgrchhS9KxACDy3jefaQnxb9f6QTSDBFmhnGa0cOIWtNAIFRAG9ToOTpi0mnEo3zFA'
+    server_url = 'https://darwin-api.sparkcognition.com/v1/'
+    api_key = ''
